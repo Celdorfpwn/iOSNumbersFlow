e4ef2f67615f338a40ba58429969bdc02b33d706
Ionut Apostol <ionutapostol@ionuts-mac-mini.local>
2016-12-08 22:20:35 +0200

    first commit

--- NumbersFlow/ViewController.swift
+++ NumbersFlow/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generateButtons()
     }
-
+    
+    func generateButtons() {
+        
+        
+        
+        let buttonSize : CGFloat = view.frame.width / 6
+        
+        let whiteSpace : CGFloat = buttonSize / 6
+        
+        var xPosition : CGFloat = whiteSpace
+        var yPosition : CGFloat = 100
+        
+        
+        for _ in 1...5 {
+            
+            for _ in 1...5 {
+                let button =  UIButton(type: UIButtonType.system)
+                button.frame = CGRect(x: xPosition,y: yPosition,width: buttonSize,height: buttonSize)
+                button.backgroundColor = UIColor.blue
+                button.setTitle("sometitle", for: UIControlState.normal)
+                
+                xPosition += buttonSize + whiteSpace
+                
+                view.addSubview(button)
+            }
+            
+            xPosition = whiteSpace
+            yPosition += buttonSize + whiteSpace
+            
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
 }
 
